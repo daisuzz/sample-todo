f90722ffa1474c9d17636ca7217007354ee1aa62
daisuzz <daisaku.rugby@gmail.com>
2020-10-10 14:10:12 +0900

    add done button

--- src/main/kotlin/com/daisuzz/samplethymeleaftodo/infrastructure/TodoMapper.kt
+++ src/main/kotlin/com/daisuzz/samplethymeleaftodo/infrastructure/TodoMapper.kt
@@ -11,7 +11,7 @@ interface TodoMapper {
     @Select("select * from todo")
     fun findAll(): List<TodoEntity>
 
-    @Select("select * from todo where #{id}")
+    @Select("select * from todo where id = #{id}")
     fun find(id: String): TodoEntity
 
     @Insert("insert into todo (id, title, detail, isDone) values (#{id}, #{title}, #{detail}, #{isDone})")
@@ -20,6 +20,6 @@ interface TodoMapper {
     @Insert("update todo set title = #{title}, detail = #{detail}, isDone = #{isDone} where id = #{id}")
     fun update(todo: TodoEntity)
 
-    @Delete("delete from todo where #{id}")
+    @Delete("delete from todo where id = #{id}")
     fun delete(id: String)
 }
