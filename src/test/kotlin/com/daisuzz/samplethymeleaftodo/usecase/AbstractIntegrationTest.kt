5c0f89b8357d0d15c0c9284483ec2f15645e6dd6
daisuzz <daisaku.rugby@gmail.com>
2021-05-30 18:14:58 +0900

    turn into container initialization being lazy

--- src/test/kotlin/com/daisuzz/samplethymeleaftodo/usecase/AbstractIntegrationTest.kt
+++ src/test/kotlin/com/daisuzz/samplethymeleaftodo/usecase/AbstractIntegrationTest.kt
@@ -9,10 +9,12 @@ abstract class AbstractIntegrationTest {
 
     companion object {
 
-        private val instance: KDockerComposeContainer = KDockerComposeContainer(File("docker-compose.yml"))
-            .withExposedService("db-server", 3306)
-
         class KDockerComposeContainer(file: File) : DockerComposeContainer<KDockerComposeContainer>(file)
+
+        private val instance: KDockerComposeContainer by lazy { defineDockerCompose() }
+
+        private fun defineDockerCompose() = KDockerComposeContainer(File("docker-compose.yml"))
+            .withExposedService("db-server", 3306)
 
         init {
             instance.start()
