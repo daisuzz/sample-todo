8cdc941eee45da6a390072aed7d1490bc0158197
daisuzz <daisaku.rugby@gmail.com>
2021-10-31 19:57:14 +0900

    セッションをDBで管理する設定を追加

--- src/main/kotlin/com/daisuzz/samplethymeleaftodo/domain/auth/TodoUser.kt
+++ src/main/kotlin/com/daisuzz/samplethymeleaftodo/domain/auth/TodoUser.kt
@@ -1,7 +1,9 @@
 package com.daisuzz.samplethymeleaftodo.domain.auth
 
+import java.io.Serializable
+
 data class TodoUser(
     val userId: String,
     val userName: String,
     val password: String
-)
+) : Serializable
