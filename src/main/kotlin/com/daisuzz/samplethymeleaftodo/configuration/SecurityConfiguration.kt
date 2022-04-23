13c6f977babb8268123ebf4065112f883befffa9
daisuzz <daisaku.rugby@gmail.com>
2022-04-23 18:11:15 +0900

    delete unused annotation

--- src/main/kotlin/com/daisuzz/samplethymeleaftodo/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/daisuzz/samplethymeleaftodo/configuration/SecurityConfiguration.kt
@@ -1,14 +1,14 @@
 package com.daisuzz.samplethymeleaftodo.configuration
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
-@EnableWebSecurity
+@Configuration
 class SecurityConfiguration {
 
     @Bean
